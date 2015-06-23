b4af9e9a00bf7467574779f11eddf60cbfc1a22a
Md Asadul Islam <asad@Mds-MacBook-Pro.local>
2015-06-22 20:21:31 -0600

    inserting data to database

--- DBConn.py
+++ DBConn.py
@@ -5,15 +5,13 @@ db = MySQLdb.connect("localhost","dbuser","dbuser","testDB" )
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
-# execute SQL query using execute() method.
-cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+#inserting data
+sql = """INSERT INTO EMPLOYEE(FIRST_NAME,LAST_NAME,AGE,SEX,INCOME)
+        VALUES('MD ASADUL','ISLAM',28,'M',1908)"""
 
-#create table
-sql = """CREATE TABLE EMPLOYEE (
-        FIRST_NAME CHAR(20) NOT NULL,
-        LAST_NAME CHAR(20),
-        AGE INT,
-        SEX CHAR(1),
-        INCOME FLOAT)"""
-cursor.execute(sql)
+try:
+    cursor.execute(sql)
+    db.commit();
+except:
+    db.rollback();
 db.close()
