dcf0033f815e35ae03618357e460a2bd64ce3975
Md Asadul Islam <asad@Mds-MacBook-Pro.local>
2015-06-22 19:51:02 -0600

    creating employee table

--- DBConn.py
+++ DBConn.py
@@ -1,17 +1,19 @@
 import MySQLdb
 # Open database connection
-db = MySQLdb.connect("localhost","root","","testDB" )
+db = MySQLdb.connect("localhost","dbuser","dbuser","testDB" )
 
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
 # execute SQL query using execute() method.
-cursor.execute("SELECT VERSION()")
+cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
 
-# Fetch a single row using fetchone() method.
-data = cursor.fetchone()
-
-print "Database version : %s " % data
-
-# disconnect from server
+#create table
+sql = """CREATE TABLE EMPLOYEE (
+        FIRST_NAME CHAR(20) NOT NULL,
+        LAST_NAME CHAR(20),
+        AGE INT,
+        SEX CHAR(1),
+        INCOME FLOAT)"""
+cursor.execute(sql)
 db.close()
